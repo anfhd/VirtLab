adcbe781c68d3892aab9c12a27522dc51dd4db0a
Ruslan <Ruslan@DESKTOP-2C7OVNH>
2024-11-22 20:08:50 +0200

    added repositories for students, projects, techs and languages

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IRepositoryManager
+    {
+        IProgrammingLanguageRepository ProgrammingLanguage { get; }
+        IProjectRepository Project { get; }
+        IStudentRepository Student { get; }
+        ITechnologyRepository Technology { get; }
+
+        void Save();
+    }
+}
